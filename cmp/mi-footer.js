a87888aa93c3b1a8164bc549bd8bce0e8a425adf
Karla-Shamady <76029576+Karla-Shamady@users.noreply.github.com>
2020-12-15 15:09:45 -0600

    Create mi-footer.js

---
+++ cmp/mi-footer.js
@@ -0,0 +1,7 @@
+class MiFooter extends HTMLElement {
+connectedCallback(){
+this.innerTHML = /* html */
+`Copyright &copy; 2020 Karla Shamady Vasquez Hernandez`;
+}
+}
+customElement.define(mi-footer", MiFooter);
